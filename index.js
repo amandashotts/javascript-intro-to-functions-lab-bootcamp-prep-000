ff8d647647015791633ca9106671b971952a1168
Amanda S <shottsan@dukes.jmu.edu>
2018-06-17 03:11:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,26 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-"bad bitch contest you in first place".toUpperCase();
+shout("Bad bitch contest u in first place <3");
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+whisper("Bad bitch contest u in first place <3");
+
+function logShout(string) {
+  console.log(string.toUpperCase)
+}
+
+logShout("Bad bitch contest u in first place <3");
+
+function logWhisper(string) {
+  return string.toLowerCase()
+}
+
+logWhisper("Bad bitch contest u in first place <3");
+
+function sayHiToGrandma(string) {
+  return string.toUpperCase
+}
